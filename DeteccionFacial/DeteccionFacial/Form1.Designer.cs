e80e64b663d2271e4ef6097c8369338a48e6f702
Martin Mercado <68834126+Martin-Mercado@users.noreply.github.com>
2023-06-29 18:59:58 -0600

    Add files via upload

---
+++ DeteccionFacial/DeteccionFacial/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DeteccionFacial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.picDetected = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetected)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCapture
+            // 
+            this.picCapture.Location = new System.Drawing.Point(12, 12);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(541, 426);
+            this.picCapture.TabIndex = 1;
+            this.picCapture.TabStop = false;
+            // 
+            // picDetected
+            // 
+            this.picDetected.Location = new System.Drawing.Point(586, 126);
+            this.picDetected.Name = "picDetected";
+            this.picDetected.Size = new System.Drawing.Size(182, 179);
+            this.picDetected.TabIndex = 2;
+            this.picDetected.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picDetected);
+            this.Controls.Add(this.picCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetected)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.PictureBox picDetected;
+    }
+}
+
---
+++ DeteccionFacial/DeteccionFacial/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace DeteccionFacial
+{
+    public partial class Form1 : Form
+    {
+
+        #region variables
+
+        private Capture videoCapture = null;
+
+        /*
+        imagen con colores específicos y tipos de profundidad.En este caso, Bgr representa
+        el tipo de color de la imagen, lo que indica que utiliza el modelo de color Azul-Verde-Rojo.
+        El tipo Byte indica que la profundidad de cada canal de color es de 8 bits, representando valores de 0 a 255.
+        */
+        private Image<Bgr, Byte> currentFrame = null;
+
+        //Matriz de pixeles vacia
+        Mat frame = new Mat();
+
+        //objeto para detección de objetos basada en clasificadores en cascada haars. En este caso se utiliza para la detección de rostros.
+        CascadeClassifier faceCascadeClassifier = new CascadeClassifier(@"..\haarcascade_frontalface_alt.xml");
+
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            videoCapture = new Capture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+        }
+
+
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            // capturar cuadro
+            videoCapture.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCapture.Width, picCapture.Height, Inter.Cubic);
+
+            //Detectar rostro
+   
+            //convertir imagen Bgr a escala de grises
+            Mat grayImage = new Mat();
+            CvInvoke.CvtColor(currentFrame, grayImage, ColorConversion.Bgr2Gray);
+
+            //Ecualización de histograma: distribución más uniforme de las intensidades de píxeles en el rango de escala de grises de la imagen
+            CvInvoke.EqualizeHist(grayImage, grayImage);
+
+            //Realizar la deteccion de rostro en grayImage usando faceCascadeClassifier (Cascade Classifier).
+            Rectangle[] faces = faceCascadeClassifier.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+
+
+
+            // renderizar cuadro en el picture box
+            foreach (var face in faces)
+            {
+                //diujar un rectangulo ale rededor de la cara actual
+                CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Green).MCvScalar, 2);
+
+                //Agregar persona
+                //asignar la cara detectada a la picture box PicDetected
+                Image<Bgr, Byte> resultImage = currentFrame.Convert<Bgr, Byte>();
+                resultImage.ROI = face;
+                picDetected.SizeMode = PictureBoxSizeMode.StretchImage;
+                picDetected.Image = resultImage.Bitmap;
+            }
+                picCapture.Image = currentFrame.Bitmap;
+        }
+    }
+}
